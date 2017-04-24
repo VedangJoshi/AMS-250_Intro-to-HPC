7ebdf6cfdf7650bb77113f910e5accce5253aa0c
Vedang Joshi <joshi.vedang94@gmail.com>
2017-04-23 22:00:36 -0700

    Completed

---
+++ Pie_calculator.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <omp.h>
+
+inline double func(double x) {
+	return (1 / (1 + x*x));
+}
+
+int main (int argc, char ** argv) {
+	int n;
+	int a = 0, b = 1;
+	int N = 100000;
+	double sum = 0;
+
+	// h
+	double h = (b-a) / (double) N;
+
+	// Calculation of π using midpoint rule 
+	#pragma omp parallel shared(a, h) private(n) reduction(+:sum)
+	{	
+		#pragma omp for schedule(dynamic, N/1000) nowait
+		for(n = 0; n < N; n++) {
+			sum += func(a + n*h);
+			//printf("\n%d", omp_get_thread_num());
+		}
+	}
+	
+	printf("π = %f", h * sum * 4);
+	
+	return EXIT_SUCCESS;
+}
